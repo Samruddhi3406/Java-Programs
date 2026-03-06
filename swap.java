4789bb8e5bf8e632eb5fc59c66461970cbf58b54
Samruddhi3406 <samruddhitalekar2006@gmail.com>
2026-03-06 11:01:35 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,18 @@
+class swap
+{
+public static void main(String[] args)
+{
+ int a=10;
+ int b=5;
+ System.out.println("Values before swapping:");
+ System.out.println("a="+a );
+ System.out.println("b=" +b);
+ int z;
+ z=a;
+ a=b;
+ b=z;
+ System.out.println("values after swapping: ");
+ System.out.println("a=" +a);
+ System.out.println("b=" +b);
+}
+}
