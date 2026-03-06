c2eecbde3fbc800051abcefb00e0f672a1d4e032
Samruddhi3406 <samruddhitalekar2006@gmail.com>
2026-03-06 11:04:45 +0530

    Add files via upload

---
+++ SDemo.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+public class SDemo {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Stack: " + stack);
+        System.out.println("Popped element: " + stack.pop());
+        System.out.println("Stack after pop: " + stack);
+    }
+}
