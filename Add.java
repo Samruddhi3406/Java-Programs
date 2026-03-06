ceecf7a40fadb0f5bdcafb3600c48c297da15796
Samruddhi3406 <samruddhitalekar2006@gmail.com>
2026-03-06 11:01:14 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,13 @@
+class Add
+{
+public static void main(String[] args)
+{
+ int a=10;
+ int b=10;
+ int c;
+ c=a+b;
+ System.out.println("Value of a:" +a);
+ System.out.println("Value of b:" +b);
+ System.out.println("Addition is:" +c);
+ }
+}
